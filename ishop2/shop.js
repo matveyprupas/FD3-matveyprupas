52a18d37132408e5bf13a722f87a701af2a02759
matveyprupas <matveyprupas@gmail.com>
2021-08-08 16:28:42 +0300

    fix: fix all of these components fix: add new choose method

--- ishop2/shop.js
+++ ishop2/shop.js
@@ -1,13 +1,53 @@
-let shopName2 = "IShop2";
-
 let Shop2 = React.createClass({
 
     displayName: "Shop2",
 
+    propTypes: {
+        shopName: React.PropTypes.string.isRequired,
+        goodsArr: React.PropTypes.array.isRequired,
+      },
+
+    getInitialState: function() {
+        let state = {};
+        for (let key in this.props.goodsArr) {
+            state[`good_${this.props.goodsArr[key].code}`] = this.props.goodsArr[key];
+            state[`good_${this.props.goodsArr[key].code}`].show = true;
+            state[`good_${this.props.goodsArr[key].code}`].choose = false;
+            state[`good_${this.props.goodsArr[key].code}`].className = "good__description";
+        };
+        return state;
+    },
+
+    chooseGood: function(obj) {
+        let keyGood = `good_${obj.code}`;
+        
+        for (let key in this.state) {
+            this.setState({[key]: {...this.state[key], className: "good__description"}});
+        }
+        
+        if (obj.choose) {
+            this.setState({[keyGood]: {...this.state[keyGood], className: "good__description choosed__good"}});
+        } else {
+            this.setState({[keyGood]: {...this.state[keyGood], className: "good__description"}});
+        }
+        // console.log("shop: ");
+        // console.log(obj);
+        // console.log(this.state[keyGood], `good_${obj.code}`);
+    },
+
     render: function() {
+        let goodsArrayDOM = [];
+        // console.log(this.state);
+
+
+        for(let key in this.state) {
+            goodsArrayDOM.push( React.createElement(Goods, {...this.state[key], cbChooseGood: this.chooseGood, key: this.state[key].code}) );
+        }
+        // console.log(goodsArrayDOM);
+
         return React.DOM.div( {className: "goods__frame"},
-            React.DOM.h1( {className: "goods__title"}, shopName2 ),
-            React.createElement(Goods, this.props ),
-            );
+            React.DOM.h1( {className: "goods__title"}, this.props.shopName ),
+            React.DOM.div( {className: "goods__wrap"}, goodsArrayDOM ),
+        );
     },
 }); 
