7125680b3f796137109b9be5fdb28a5772719ba0
matveyprupas <matveyprupas@gmail.com>
2021-08-17 14:03:13 +0300

    feat: start and done br2jsx task

---
+++ br2jsx/src/br2jsx.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './br2jsx.css';
+
+class BR2JSX extends React.Component {
+
+  render() {
+      let regEx = /<br\s?\/?>/i;
+      let resultText = this.props.text.split(regEx).map((el, i) => {
+          if (!i) return el;
+            let res = [<br />, el];
+            return res;
+        }).flat(1);
+      console.log(resultText);
+    return <div className = "br2jsx">{resultText}</div>
+  }
+}
+
+export default BR2JSX;
