9aa183a41309b30dabe52d72144aa4178fe2547e
matveyprupas <matveyprupas@gmail.com>
2021-08-17 10:23:30 +0300

    fix: change key attribute

--- rainbow-frame/src/rainbow-frame.js
+++ rainbow-frame/src/rainbow-frame.js
@@ -5,7 +5,7 @@ class RainbowFrame extends React.Component {
 
   render() {
     let result = this.props.colors.reduce( (acc, el, i) => {
-        acc = <div key = {el} style={{borderColor: el}} className="rainbow-frame-wrap">{acc}</div>;
+        acc = <div key = {i} style={{borderColor: el}} className="rainbow-frame-wrap">{acc}</div>;
         return acc;
     }, this.props.children);
 
