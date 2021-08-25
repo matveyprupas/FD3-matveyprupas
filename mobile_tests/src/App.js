490f3b1e667c26f41d9dfae96c9d2e762b306032
matveyprupas <matveyprupas@gmail.com>
2021-08-25 07:49:59 +0300

    fix: start write tests

--- mobile_tests/src/App.js
+++ mobile_tests/src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import MobileCompany from './components/MobileCompany';
+import clientsHash from './clientsHash.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MobileCompany name={clientsHash.companyName} clients={clientsHash.clientsArr} />
   );
 }
 
--- mobile_tests/src/App.test.js
+++ mobile_tests/src/App.test.js
@@ -1,8 +1,60 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import MobileCompany from './components/MobileCompany';
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer';
+import clientsHash from './clientsHash.json';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
+
+test('работа MobileCompany', () => {
+
+  // создаём тестовую версию компонента
+  const component = renderer.create(
+    <MobileCompany name={clientsHash.companyName} clients={clientsHash.clientsArr} />
+  );
+
+  // получаем снэпшот (HTML-снимок) компонента для сверки, что вёрстка не испортилась
+  let componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  // найдём в вёрстке компонента саму кнопку
+  // console.log(component.root);
+  const buttonElem = component.root.find( el => el.type === 'button' && el.props.value === 'All' ); 
+  // и "нажмём" на неё
+  buttonElem.props.onClick();
+  // component.root.find('button').simulate('click');
+
+
+  // получаем уже изменённый снэпшот
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  // "нажмём" кнопку ещё раз
+  buttonElem.props.onClick();
+  
+  // и получаем окончательный снэпшот
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+  
+  /*
+  // можно эмулировать события, передавая в качестве объекта события то что нам нужно:
+  wrapper.find('select').simulate('change', {
+    target: { value: "hello" },
+  });
+  */
+
+});
+
+
+
+
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
